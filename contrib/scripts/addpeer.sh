24482effebbe723806129acad4f25a1f48502c58
Tab Fitts <tfitts@spryservers.net>
2018-02-10 20:44:34 -0700

    Update script to generate & print keys and add peer to server.
    Generate keys using https://github.com/hobby-kube/provisioning/blob/master/security/wireguard/scripts/gen_keys.sh

--- contrib/scripts/addpeer.sh
+++ contrib/scripts/addpeer.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-wg set wg0 peer $CLIENT_PUBKEY allowed-ips $CLIENT_IP/32
+set -e
+
+private_key=$(wg genkey)
+public_key=$(echo $private_key | wg pubkey)
+
+sudo wg set wg0 peer $public_key allowed-ips $client_ip/32
+
+jq -n --arg private_key "$private_key" \
+  --arg public_key "$public_key" \
+  '{"private_key":$private_key,"public_key":$public_key}'
